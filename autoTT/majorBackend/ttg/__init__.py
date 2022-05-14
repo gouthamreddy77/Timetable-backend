895a9136ba99e9eb2d05a6fb4991f5076fc695db
mohdrab01 <mohdrab01@gmail.com>
2022-05-14 15:47:58 +0530

    added cors...

--- autoTT/majorBackend/ttg/__init__.py
+++ autoTT/majorBackend/ttg/__init__.py
@@ -1,10 +1,12 @@
 from flask import Flask
+from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager,login_user,logout_user,current_user,login_required
 from datetime import datetime
 
 
 app = Flask(__name__)
+CORS(app)
 
 app.config['SECRET_KEY'] ='webdev'
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
